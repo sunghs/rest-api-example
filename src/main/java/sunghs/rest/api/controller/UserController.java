c50eba557edf5ecc1104193428a006f85e8f3b19
성현수 <hyunsu.sung@homeplus.co.kr>
2020-07-07 19:34:15 +0900

    ApiResponse, RequestMapping 수정

--- src/main/java/sunghs/rest/api/controller/UserController.java
+++ src/main/java/sunghs/rest/api/controller/UserController.java
@@ -3,14 +3,28 @@ package sunghs.rest.api.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import sunghs.rest.api.model.UserInfo;
 import sunghs.rest.api.service.UserService;
 
 @Api(value = "UserController", tags = "유저 컨트롤러")
+@ApiResponses(value = {
+    @ApiResponse(code = 200, message = "OK, 성공"),
+    @ApiResponse(code = 201, message = "Created, 리소스 생성 성공"),
+    @ApiResponse(code = 307, message = "Temporary Redirect, URI가 변경 됨"),
+    @ApiResponse(code = 401, message = "Unauthorized, 인증되지 않음"),
+    @ApiResponse(code = 403, message = "Forbidden, 권한이 없음"),
+    @ApiResponse(code = 404, message = "Api Not Found, 없는 API 주소"),
+    @ApiResponse(code = 500, message = "Internal Server Error, 서버 에러"),
+    @ApiResponse(code = 503, message = "Service Unavailable, 서비스 요청 불가"),
+    @ApiResponse(code = 504, message = "Gateway Timeout, 응답시간 초과")
+})
 @RestController
-@RequestMapping("/user")
+@RequestMapping(value = "/user", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.TEXT_PLAIN_VALUE})
 @RequiredArgsConstructor
 public class UserController {
 
