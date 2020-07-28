fba50fc9fd2094c51586283b8b81483b02c62cb1
sunghs <sunghs9071@naver.com>
2020-07-28 21:58:18 +0900

    WebConfig 추가 및 properties 파일을 yml로 변경

---
+++ src/main/java/sunghs/rest/api/config/WebConfig.java
@@ -0,0 +1,14 @@
+package sunghs.rest.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("redirect:/swagger-ui.html");
+    }
+}
