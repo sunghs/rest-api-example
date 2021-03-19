1b6c78d4ee27d3e229b451a457faee4ef5836d96
sunghs <sunghs9071@naver.com>
2021-03-19 10:53:53 +0900

    JUnit 관련 테스트 추가2

--- src/test/java/sunghs/rest/api/JUnitTests.java
+++ src/test/java/sunghs/rest/api/JUnitTests.java
@@ -3,8 +3,10 @@ package sunghs.rest.api;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -38,10 +40,26 @@ public class JUnitTests {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {
-        "a", "b", "c", "d"
+    @DisplayName("파라미터를 넣어 테스트")
+    @ValueSource(ints = {
+        1, 2, 3, 4, 5
     })
-    public void argsTest(final String s) {
-        log.info("argsTest : {}", s);
+    public void argsTest(final Integer i) {
+        Assertions.assertTrue(convert(i));
+        Assertions.assertEquals(3, i);
+    }
+
+    // i 값이 5 미만이라면 true를 줍니다.
+    private boolean convert(Integer i) {
+        return i < 5;
+    }
+
+    @Test
+    public void exceptionTest() {
+        Assertions.assertThrows(NumberFormatException.class, this::exceptionGenerator);
+    }
+
+    private void exceptionGenerator() {
+        throw new NumberFormatException();
     }
 }
