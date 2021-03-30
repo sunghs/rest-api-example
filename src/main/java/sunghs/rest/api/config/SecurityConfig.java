a108ceae3584e7bd39a53a9f63fb675c3066d0f2
sunghs <sunghs9071@naver.com>
2021-03-30 11:25:03 +0900

    roles 와 authorities 오류가 있어 수정

--- src/main/java/sunghs/rest/api/config/SecurityConfig.java
+++ src/main/java/sunghs/rest/api/config/SecurityConfig.java
@@ -40,8 +40,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-            .antMatchers("/swagger-ui.html").hasAnyAuthority("USER", "ADMIN")
-            .antMatchers("/admin/**").hasAuthority("ADMIN")
+            .antMatchers("/swagger-ui.html").hasAnyRole("USER", "ADMIN")
+            .antMatchers("/admin/**").hasRole("ADMIN")
             .anyRequest()
             .authenticated()
             .and()
